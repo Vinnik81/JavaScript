ad6905670804072ef93fd62fedf627830792de5c
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-02-19 17:37:10 +0600

    commit - a Forms

---
+++ Forms/script.js
@@ -0,0 +1,124 @@
+// let submitButton = document.querySelector('#submit');
+
+
+// submitButton.addEventListener('click', function() {
+//     console.log('Test');
+//     event.preventDefault();
+// })
+
+
+//let form = document.querySelector('#textForm');
+
+
+// form.addEventListener('submit', function() {
+//     event.preventDefault();
+
+//      // console.log(document.forms.myForm.text.value);
+
+// })
+
+
+
+
+
+// document.forms.myForm.text.addEventListener('change', function() {
+//      console.log(`Change : ${event.target.value}`);
+// })
+
+
+
+// document.forms.myForm.text.addEventListener('mousemove', function() {
+//     console.log(`Ты навел на инпут`);
+// })
+
+// document.forms.myForm.text.addEventListener('input', function() {
+//     // if (event.target.value.indexOf('@') > 0) {
+//     //     console.log("OK");
+//     // } else {
+//     //     console.log('BAD');
+//     // }
+
+//     // console.log(`Input : ${event.target.value}`);
+//     // event.target.classList = '';
+
+
+//     // if (event.target.value.includes('@mail.ru') ||
+//     //     event.target.value.includes('@list.ru') ||
+//     //     event.target.value.includes('@bk.ru')) {
+//     //     event.target.classList.add('valid');
+//     //     event.target.classList.remove("invalid");
+//     // } else {
+//     //     event.target.classList.add('invalid');
+//     //     event.target.classList.remove("valid");
+//     // }
+
+
+//     if (event.target.value.match('^[a-z]{1,10}$')) {
+//         console.log("Yes");
+//     } else {
+//         console.log('No');
+//     }
+
+
+
+// })
+
+
+// document.forms.myForm.text.addEventListener('keyup', function() {
+//       console.log(`keyup : ${event.key}`);
+// })
+
+// document.forms.myForm.text.addEventListener('keydown', function() {
+//      console.log(`keydown : ${event.key}`);
+//     //if (event.key == 'f' || event.key == 'F') {
+//    // if (event.key.toLowerCase() == 'f') {
+
+
+
+
+
+//     // for (let i = 0; i < 10; i++) {
+//     //     if (event.key == i) event.preventDefault();
+//     // }
+
+//     // if ("1234567890".includes(event.key)) {
+//     //     event.preventDefault();
+//     // }
+
+    
+
+//     // Hello my frend
+//     // frend
+
+//                 console.log(event);
+// })
+
+
+
+// document.forms.myForm.text.addEventListener('focus', function() {
+
+
+//     if (event.target.value.match('^[a-z]{1,10}$')) {
+//         event.target.classList.add('valid');
+//         event.target.classList.remove("invalid");
+//         console.log("Yes");
+//     } else {
+//         console.log('No');
+//         event.target.classList.add('valid');
+//         event.target.classList.remove("invalid");
+//     }
+// })
+
+
+// document.forms.myForm.text.addEventListener('blur', function() {
+
+//     if (event.target.value.match('^[a-z]{1,10}$')) {
+//         event.target.classList.add('valid');
+//         event.target.classList.remove("invalid");
+//         console.log("Yes");
+//     } else {
+//         console.log('No');
+//         event.target.classList.remove('valid');
+//         event.target.classList.add("invalid");
+//     }
+// })
